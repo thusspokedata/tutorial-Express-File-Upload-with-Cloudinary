086ae567810d806e2bf81f19368a696281e93909
Antonio Saleme <79271990+thusspokedata@users.noreply.github.com>
2022-08-02 12:33:42 +0200

    done

---
+++ myReference.md
@@ -0,0 +1,1 @@
+npm install cloudinary multer multer-storage-cloudinary
---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("movie-project JS imported successfully!");
+  },
+  false
+);
